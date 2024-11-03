ea3d730553f9244bcc61d30b5a6dc75156e7a87f
Caio Lima <todaocaio@gmail.com>
2024-11-03 19:10:36 -0300

    update do css da tela 3 do motoboy, do html do detalhes-do-pedido, do app.config.ts e do app.routes.ts

--- dish-app/src/app/app.config.ts
+++ dish-app/src/app/app.config.ts
@@ -9,6 +9,12 @@ import { PageBowlComponent } from './components/page-bowl/page-bowl.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { CarrinhoComponent } from './components/carrinho/carrinho.component';
 import { DetalhesDoPedidoComponent } from './components/detalhes-do-pedido/detalhes-do-pedido.component';
+import { TelaMotoboy1Component } from './components/tela-motoboy1/tela-motoboy1.component';
+import { TelaMotoboy2Component } from './components/tela-motoboy2/tela-motoboy2.component';
+import { TelaMotoboy3Component } from './components/tela-motoboy3/tela-motoboy3.component';
+import { TelaMotoboy4Component } from './components/tela-motoboy4/tela-motoboy4.component';
+import { TelaMotoboy5Component } from './components/tela-motoboy5/tela-motoboy5.component';
+
 
 const routes: Routes = [
   { path: '', component: DishListComponent },
@@ -17,11 +23,19 @@ const routes: Routes = [
   { path: 'page-bowl', component: PageBowlComponent },
   { path: 'carrinho', component: CarrinhoComponent },
   { path: 'detalhes-do-pedido', component: DetalhesDoPedidoComponent},
+  { path: 'tela-motoboy1', component: TelaMotoboy1Component},
+  { path: 'tela-motoboy2', component: TelaMotoboy2Component},
+  { path: 'tela-motoboy3', component: TelaMotoboy3Component},
+  { path: 'tela-motoboy4', component: TelaMotoboy4Component},
+  { path: 'tela-motoboy5', component: TelaMotoboy5Component},
+  
+  
+
 ];
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule), provideAnimationsAsync(), provideAnimationsAsync()
+    importProvidersFrom(HttpClientModule), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync()
   ]
 };
