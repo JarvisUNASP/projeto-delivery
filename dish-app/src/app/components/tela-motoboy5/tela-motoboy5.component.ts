12947f7bef3e56b7dbeafbfe6b9fab029664c45a
Caio Lima <todaocaio@gmail.com>
2024-11-03 14:29:15 -0300

    Adicionado a quinta e última tela do motoboy

---
+++ dish-app/src/app/components/tela-motoboy5/tela-motoboy5.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-tela-motoboy5',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './tela-motoboy5.component.html',
+  styleUrl: './tela-motoboy5.component.css'
+})
+export class TelaMotoboy5Component {
+
+}
