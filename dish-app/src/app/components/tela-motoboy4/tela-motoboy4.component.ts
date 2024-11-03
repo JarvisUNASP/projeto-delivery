5b63b625c4405ac52489ffb88f90cf4281027394
Caio Lima <todaocaio@gmail.com>
2024-11-03 14:24:41 -0300

    Adicionada a quarta tela do motoboy

---
+++ dish-app/src/app/components/tela-motoboy4/tela-motoboy4.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-tela-motoboy4',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './tela-motoboy4.component.html',
+  styleUrl: './tela-motoboy4.component.css'
+})
+export class TelaMotoboy4Component {
+
+}
