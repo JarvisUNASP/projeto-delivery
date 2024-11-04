ce98c3988e5767ba754f74480320bbdf201cff5a
Gabriella <166776181+GabriellaSilva33A@users.noreply.github.com>
2024-11-04 13:40:48 -0300

    Melhorias nas telas do cliente

---
+++ dish-app/src/app/components/inicial-cliente/inicial-cliente.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-inicial-cliente',
+  standalone: true,
+  imports: [ RouterModule ],
+  templateUrl: './inicial-cliente.component.html',
+  styleUrl: './inicial-cliente.component.css'
+})
+export class InicialClienteComponent {
+
+}
